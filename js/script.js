ea2b3ea77ffc24e2788741e0b35be4c3fc891010
Sahid Rahman <sahidrahman404@gmail.com>
2021-12-31 10:11:29 +0700

    delete unnecesaary comments

--- js/script.js
+++ js/script.js
@@ -5,19 +5,9 @@ yearSelect.textContent = currentYear
 
 // Make Mobile Navigation Work
 
-// const navList = document.querySelector(".nav__list");
-// const navMobile = document.querySelector(".nav__mobile");
-// const mobileIconMenu = document.querySelector(".nav__mobile ion-icon[name='menu-outline']")
-// const mobileIconClose = document.querySelector(".nav__mobile ion-icon[name='close-outline']")
-
 const navOpenSelect = document.querySelector('.nav__mobile')
-// const navCloseSelect = document.querySelector(".nav__mobile ion-icon[name='close-outline']")
 const headerSelect = document.querySelector('.header')
 
 navOpenSelect.addEventListener('click', function () {
   headerSelect.classList.toggle('nav__open')
 })
-//
-// navCloseSelect.addEventListener("click", function(){
-//   headerSelect.classList.remove("nav__open")
-// })
